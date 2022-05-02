1036fd2c4ac117ff60439ab9dcb13a48ec5e1946
Mykhailo Khranovskyi <mykhailo.khranovskyi@djangostars.com>
2022-05-02 13:28:13 +0300

    add web_service.py updated conftest.py, test_testcases.py

--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -19,8 +19,15 @@ def test_add_new_tc(desktop_app_auth, name, description):
 
 def test_testcase_does_not_exist(desktop_app_auth):
     desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists('qwerty')
+    assert not desktop_app_auth.test_cases.check_test_exists('qwerty')
 
 
-def test_git():
-    assert True
+def test_delete_test_case(desktop_app_auth, get_web_service):
+    test_name = 'test for delete'
+    get_web_service.create_test(test_name, 'delete me pls')
+    desktop_app_auth.navigate_to('Test Cases')
+    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    desktop_app_auth.test_cases.delete_test_by_name(test_name)
+    assert not desktop_app_auth.test_cases.check_test_exists(test_name)
+
+
